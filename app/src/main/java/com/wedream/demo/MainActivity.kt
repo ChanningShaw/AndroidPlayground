3396112cb397581e95499f89c4bcbddf1fd634e1
cquxcm <454785419@qq.com>
2020-08-30 12:15:42 +0800

    resume last resumed

--- app/src/main/java/com/wedream/demo/MainActivity.kt
+++ app/src/main/java/com/wedream/demo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.wedream.demo.app.MyApplication
 import com.wedream.demo.category.CategoryAdapter
 import com.wedream.demo.common.CommonAdapter
 import com.wedream.demo.render.DrawTextDemoActivity
@@ -24,15 +25,23 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val lastCom = MyApplication.ApplicationHolder.instance.getLastResumeActivity()
+        if (lastCom != componentName) {
+            val intent = Intent()
+            intent.component = lastCom
+            startActivity(intent)
+        }
         setContentView(R.layout.activity_main)
         val rv = findViewById<RecyclerView>(R.id.main_rv)
         val adapter = CategoryAdapter(this)
-        rv.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        rv.layoutManager =
+            androidx.recyclerview.widget.LinearLayoutManager(this, RecyclerView.VERTICAL, false)
 
         adapter.setData(data)
         rv.adapter = adapter
 
-        adapter.setItemClickListener(object : CommonAdapter.OnItemClickListener<Class<*>, CategoryAdapter.Holder> {
+        adapter.setItemClickListener(object :
+            CommonAdapter.OnItemClickListener<Class<*>, CategoryAdapter.Holder> {
             override fun onItemClick(data: Class<*>, holder: CategoryAdapter.Holder, pos: Int) {
                 val intent = Intent(this@MainActivity, data)
                 startActivity(intent)
---
+++ app/src/main/java/com/wedream/demo/app/MyApplication.kt
@@ -0,0 +1,67 @@
+package com.wedream.demo.app
+
+import android.app.Activity
+import android.app.Application
+import android.content.ComponentName
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import com.wedream.demo.MainActivity
+import com.wedream.demo.app.MyApplication.ApplicationHolder.instance
+
+class MyApplication : Application() {
+
+    companion object {
+        const val APP_SP_NAME = "app_sp"
+        const val KEY_LAST_RESUME_ACTIVITY = "key_last_resume_activity"
+    }
+
+    object ApplicationHolder {
+        lateinit var instance: MyApplication
+    }
+
+    private lateinit var appSp: SharedPreferences
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        appSp = getSharedPreferences("app_sp", Context.MODE_PRIVATE)
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            override fun onActivityPaused(activity: Activity) {
+            }
+
+            override fun onActivityStarted(activity: Activity) {
+            }
+
+            override fun onActivityDestroyed(activity: Activity) {
+            }
+
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+            }
+
+            override fun onActivityStopped(activity: Activity) {
+            }
+
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+            }
+
+            override fun onActivityResumed(activity: Activity) {
+                val value =
+                    "${activity.componentName.packageName}/${activity.componentName.className}"
+                appSp.edit()
+                    .putString(KEY_LAST_RESUME_ACTIVITY, value)
+                    .apply()
+            }
+        })
+    }
+
+    fun getLastResumeActivity(): ComponentName {
+        val componentString = appSp.getString(KEY_LAST_RESUME_ACTIVITY, "")
+        return if (componentString.isNullOrEmpty()) {
+            ComponentName(this, MainActivity::class.java)
+        } else {
+            val splits = componentString.split("/")
+            ComponentName(splits[0], splits[1])
+        }
+    }
+}
