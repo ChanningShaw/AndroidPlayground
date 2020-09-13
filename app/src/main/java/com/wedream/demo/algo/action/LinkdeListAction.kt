9658fa1e8f3253410fe52aceaffd9898c005024a
cquxcm <454785419@qq.com>
2020-09-13 23:01:43 +0800

    提交遗漏的文件

---
+++ app/src/main/java/com/wedream/demo/algo/action/LinkdeListAction.kt
@@ -0,0 +1,6 @@
+package com.wedream.demo.algo.action
+
+import com.wedream.demo.algo.structure.LinkedList
+
+class MoveAction(var node: LinkedList.Node<Int>?) : AlgorithmAction.LinkedListAction()
+class DeleteAction(var node: LinkedList.Node<Int>?) : AlgorithmAction.LinkedListAction()
