db0410bf4f59a0fcbb086c5f33332784f55a2e28
xiaochunming <xiaochunming@kuaishou.com>
2020-10-02 10:42:22 +0800

    more Rxajva Demo

--- app/src/main/java/com/wedream/demo/concurrent/rxjava/RxJavaDemoActivity.kt
+++ app/src/main/java/com/wedream/demo/concurrent/rxjava/RxJavaDemoActivity.kt
@@ -9,6 +9,7 @@ import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
+import io.reactivex.functions.BiFunction
 import io.reactivex.schedulers.Schedulers
 
 
@@ -52,6 +53,75 @@ class RxJavaDemoActivity : AppCompatActivity() {
                 //订阅者在主线程中
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(observer)
+        }
+        findViewById<Button>(R.id.bt_chain).setOnClickListener {
+            Observable
+                .fromCallable { "ddddd" }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { log { it } }
+        }
+        findViewById<Button>(R.id.bt_filter).setOnClickListener {
+            Observable.create<Int> {
+                it.onNext(1)
+                it.onNext(2)
+                it.onNext(3)
+                it.onNext(4)
+                it.onNext(5)
+            }.filter {
+                it % 2 == 0
+            }.subscribe {
+                log { it }
+            }
+        }
+        findViewById<Button>(R.id.bt_map).setOnClickListener {
+            Observable.create<Int> {
+                it.onNext(1)
+                it.onNext(2)
+                it.onNext(3)
+                it.onNext(4)
+                it.onNext(5)
+            }.map {
+                it.toFloat()
+            }.subscribe {
+                log { it }
+            }
+        }
+        findViewById<Button>(R.id.bt_flat_map).setOnClickListener {
+            Observable.create<Int> {
+                it.onNext(1)
+                it.onNext(2)
+                it.onNext(3)
+                it.onNext(4)
+                it.onNext(5)
+            }.flatMap {
+                Observable.fromCallable {
+                    it.toFloat()
+                }
+            }.subscribe {
+                log { it }
+            }
+        }
+        findViewById<Button>(R.id.bt_zip).setOnClickListener {
+            val o1 = Observable.create<Int> {
+                it.onNext(1)
+                it.onNext(2)
+                it.onNext(3)
+                it.onNext(4)
+                it.onNext(5)
+            }
+            val o2 = Observable.create<Int> {
+                it.onNext(6)
+                it.onNext(7)
+                it.onNext(8)
+                it.onNext(9)
+                it.onNext(10)
+            }
+            Observable.zip(o1, o2, BiFunction<Int, Int, String> { t1, t2 ->
+                "$t1,$t2"
+            }).subscribe {
+                log { it }
+            }
         }
     }
 }
