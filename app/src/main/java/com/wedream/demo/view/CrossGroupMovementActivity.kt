558e57b7353f41d42f06d8e41e0eb451bdfd4d97
xiaochunming <xiaochunming@kuaishou.com>
2021-04-13 20:12:01 +0800

    动态添加view

--- app/src/main/java/com/wedream/demo/view/CrossGroupMovementActivity.kt
+++ app/src/main/java/com/wedream/demo/view/CrossGroupMovementActivity.kt
@@ -7,36 +7,41 @@ import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.wedream.demo.R
+import com.wedream.demo.util.AndroidUtils
 import com.wedream.demo.util.LogUtils.log
 import com.wedream.demo.view.multitrack.SliderView
 import com.wedream.demo.view.multitrack.base.ElementView
 import com.wedream.demo.view.multitrack.overlap
+import kotlin.math.round
 
 class CrossGroupMovementActivity : AppCompatActivity() {
 
     lateinit var container: FrameLayout
-    private lateinit var group1: FrameLayout
-    lateinit var group2: FrameLayout
 
-    private val groupRectMap = mutableMapOf<View, ViewInfo>()
-    private val group1RectMap = mutableMapOf<Int, ViewInfo>()
-    private val group2RectMap = mutableMapOf<Int, ViewInfo>()
+    private val totalRectMap = mutableMapOf<View, ViewInfo>()
+    private val groups = mutableListOf<MutableMap<Int, ViewInfo>>()
+    private val totalGroupMap = mutableMapOf<Int, ViewGroup>()
 
     private var movingStart = false
     private var canPlace = false
     private var originRect = Rect()
     private var newGroupTipsView: View? = null
+
     // 正在操作的group
     private var operatingGroup: ViewGroup? = null
+    private var operatingView: View? = null
+
+    private val GROUP_HEIGHT = AndroidUtils.dip2pix(100)
 
     private var elementEventListener = object : ElementView.ElementEventListener {
 
         override fun onActionDown(view: ElementView) {
+            operatingView = view
         }
 
         override fun onMove(view: ElementView, deltaX: Int, deltaY: Int) {
             if (movingStart) {
-                val info = groupRectMap[view] ?: return
+                val info = totalRectMap[view] ?: return
                 info.rect.offset(deltaX, deltaY)
                 handleAdsorption(info)
                 log { "onMove rect = ${info.rect}" }
@@ -55,44 +60,48 @@ class CrossGroupMovementActivity : AppCompatActivity() {
                 movingStart = false
                 view.z -= 10
                 (view.parent as ViewGroup).z -= 10
-                if (!canPlace) {
+                if (newGroupTipsView?.isAttachedToWindow == true) {
+                    operatingGroup?.let {
+                        hideNewGroupTipsView(it)
+                    }
+                    totalRectMap[operatingView]?.let {
+                        log { "newGroup at ${it.rect}" }
+                        checkNewGroupPos(it.getGlobalRect())
+                    }
+                } else if (!canPlace) {
                     view.alpha = 1.0f
-                    groupRectMap[view]?.rect?.set(originRect)
+                    totalRectMap[view]?.rect?.set(originRect)
                     layoutView(view, originRect)
                 } else {
                     // 如果可以放置，先找出移到了哪个容器里面
-                    val info = groupRectMap[view] ?: return
+                    val info = totalRectMap[view] ?: return
 //                    log { "rect = ${info.rect}" }
                     val id = view.tag as Int
-                    val rect1 = Rect(group1.left, group1.top, group1.right, group1.bottom)
-                    val globalRect = info.getGlobalRect()
-                    if (rect1.contains(globalRect)) {
-                        log { "在 group1 中" }
-                        if (!group1RectMap.containsKey(id)) {
-                            group2RectMap.remove(id)?.let {
-                                group1RectMap[id] = it
-                                it.rect.set(globalRect)
-                                it.rect.offset(0, -group1.top)
-                                it.offset = group1.top
-                                fullUpdate()
-                            }
-                        }
-                    }
-                    val rect2 = Rect(group2.left, group2.top, group2.right, group2.bottom)
-                    if (rect2.contains(globalRect)) {
-                        log { "在 group2 中" }
-                        if (!group2RectMap.containsKey(id)) {
-                            group1RectMap.remove(id)?.let {
-                                group2RectMap[id] = it
-                                it.rect.set(globalRect)
-                                it.rect.offset(0, -group2.top)
-                                it.offset = group2.top
-                                fullUpdate()
+                    for (i in groups.indices) {
+                        val group = totalGroupMap[i] ?: continue
+                        val rect = Rect(group.left, group.top, group.right, group.bottom)
+                        val globalRect = info.getGlobalRect()
+                        if (rect.contains(globalRect)) {
+                            log { "在 group1 中" }
+                            if (groups[i].containsKey(id)) {
+                                // 在其他groups中移除
+                                for (j in groups.indices) {
+                                    if (groups[j].containsKey(id)) {
+                                        groups[j].remove(id)?.let {
+                                            groups[i][id] = it
+                                            it.rect.set(globalRect)
+                                            it.rect.offset(0, -group.top)
+                                            it.offset = group.top
+                                            fullUpdate()
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
                 }
             }
+            operatingView = null
         }
 
         override fun onLongPress(view: ElementView) {
@@ -101,7 +110,7 @@ class CrossGroupMovementActivity : AppCompatActivity() {
                 movingStart = true
                 view.z += 10
                 (view.parent as ViewGroup).z += 10
-                originRect.set(groupRectMap[view]?.rect!!)
+                originRect.set(totalRectMap[view]?.rect!!)
             }
         }
 
@@ -113,11 +122,7 @@ class CrossGroupMovementActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cross_group_move)
         container = findViewById(R.id.container)
-        group1 = findViewById(R.id.group1)
-        group2 = findViewById(R.id.group2)
-        group1.tag = 1
-        group2.tag = 2
-        group1.post {
+        container.post {
             initRects()
         }
     }
@@ -125,24 +130,39 @@ class CrossGroupMovementActivity : AppCompatActivity() {
     private fun initRects() {
         var offset = 0
         val width = 150
+        val m1 = mutableMapOf<Int, ViewInfo>()
         for (i in 1..5) {
-            val rect = Rect(offset, 0, offset + width, group1.height)
-            group1RectMap[i] = ViewInfo(rect, group1.top)
+            val rect = Rect(offset, 0, offset + width, GROUP_HEIGHT)
+            m1[i] = ViewInfo(rect, 0)
             offset += width
         }
+        groups.add(m1)
+
+        val m2 = mutableMapOf<Int, ViewInfo>()
         offset = 0
         for (i in 6..10) {
-            val rect = Rect(offset, 0, offset + width, group2.height)
-            group2RectMap[i] = ViewInfo(rect, group2.top)
+            val rect = Rect(offset, 0, offset + width, GROUP_HEIGHT)
+            m2[i] = ViewInfo(rect, GROUP_HEIGHT)
             offset += width
         }
+        groups.add(m2)
         fullUpdate()
     }
 
     private fun fullUpdate() {
-        groupRectMap.clear()
-        addViews(group1, group1RectMap)
-        addViews(group2, group2RectMap)
+        totalRectMap.clear()
+        var offset = 0
+        for ((i, m) in groups.withIndex()) {
+            val group = FrameLayout(this)
+            group.clipChildren = false
+            group.setBackgroundResource(R.drawable.simple_border)
+            val params = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, GROUP_HEIGHT)
+            params.topMargin = offset
+            container.addView(group, params)
+            totalGroupMap[i] = group
+            addViews(group, m)
+            offset += GROUP_HEIGHT
+        }
     }
 
     private fun addViews(group: ViewGroup, map: MutableMap<Int, ViewInfo>) {
@@ -161,7 +181,7 @@ class CrossGroupMovementActivity : AppCompatActivity() {
                     view.setBackgroundResource(R.color.red_dot_color)
                 }
             }
-            groupRectMap[view] = e.value
+            totalRectMap[view] = e.value
             view.tag = e.key
             val params = FrameLayout.LayoutParams(e.value.rect.width(), e.value.rect.height())
             params.topMargin = e.value.rect.top
@@ -172,7 +192,7 @@ class CrossGroupMovementActivity : AppCompatActivity() {
 
     private fun checkOverlap(view: View, info: ViewInfo): Boolean {
         val globalRect = info.getGlobalRect()
-        for (e in groupRectMap) {
+        for (e in totalRectMap) {
             if (e.key == view) {
                 continue
             }
@@ -189,8 +209,9 @@ class CrossGroupMovementActivity : AppCompatActivity() {
         // 转换成全局坐标
         rect.offset(0, info.offset)
         restrictWithin(rect, container)
-        adsorbContainerBorder(rect, group1)
-        adsorbContainerBorder(rect, group2)
+        for (e in totalGroupMap) {
+            adsorbContainerBorder(rect, e.value)
+        }
         // 转回容器内坐标
         rect.offset(0, -info.offset)
         info.rect.set(rect)
@@ -227,6 +248,14 @@ class CrossGroupMovementActivity : AppCompatActivity() {
         } else {
             hideNewGroupTipsView(viewGroup)
         }
+    }
+
+    private fun checkNewGroupPos(rect: Rect): Int {
+        val centerY = rect.centerY()
+        val height = rect.height()
+        val index = round(centerY * 1.0 / height)
+        log { "index = $index" }
+        return index.toInt()
     }
 
     private fun restrictWithin(rect: Rect, viewGroup: ViewGroup) {
