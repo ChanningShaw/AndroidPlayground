03f715831e68241016b7baed1998ad49c11d8688
xiaochunming <xiaochunming@kuaishou.com>
2021-07-26 16:53:33 +0800

    add ConstraintLayout

---
+++ app/src/main/java/com/wedream/demo/view/ConstraintLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.wedream.demo.view
+
+import android.os.Bundle
+import com.wedream.demo.R
+import com.wedream.demo.app.BaseActivity
+
+class ConstraintLayoutActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.acitivity_constraint_layout)
+    }
+}
