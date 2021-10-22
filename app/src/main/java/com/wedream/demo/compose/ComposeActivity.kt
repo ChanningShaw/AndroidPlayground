a8d56918add9820137ec3e82053b68fed706432b
xiaochunming <xiaochunming@kuaishou.com>
2021-10-22 16:23:57 +0800

    upgrade AGP，introduce compose

---
+++ app/src/main/java/com/wedream/demo/compose/ComposeActivity.kt
@@ -0,0 +1,14 @@
+package com.wedream.demo.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class ComposeActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
